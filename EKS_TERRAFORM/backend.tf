1cd5a8c5f8fbff980f81c04d92364737c6051d48
shivyk17 <106329173+shivyk17@users.noreply.github.com>
2025-07-13 20:21:19 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "sureshk8ks" # Replace with your actual S3 bucket name
+    bucket = "shiv-hosterproject" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-1"
   }
